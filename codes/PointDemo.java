cb1fae640acac0060c2b82e94c633f70f6dd9b43
asrita14 <88427625+asrita14@users.noreply.github.com>
2021-09-23 16:13:00 +0530

    Add files via upload

---
+++ codes/PointDemo.java
@@ -0,0 +1,25 @@
+class Point{
+    int x; int y;
+    void setX( int x){
+        x = (x> 79 ? 79 : (x < 0 ? 0 : x)); 
+    }
+    void setY( int y){
+        y = (y> 79 ? 79 : (y < 0 ? 0 : y));
+    }
+    int getX(){
+        return x;}
+    int getY(){
+        return y;}
+    
+}
+class PointDemo {
+    public static void main(String args[])
+    {
+        Point p1= new Point();
+        p1.setX(-22);
+        p1.setY(144);
+        System.out.println("the value of x is :"+p1.getX());
+        System.out.println("the value of y is :"+p1.getY());
+        
+    }
+}
